2afd9e99b2ff610958f8fd90fb58e8513c5a9abc
rafal0502 <rafalchojnacki0502@gmail.com>
2018-05-06 15:39:15 +0200

    Change start url

--- Sparky spider RentIT/RentIT_crawler/RentIT_crawler/spiders/rentit.py
+++ Sparky spider RentIT/RentIT_crawler/RentIT_crawler/spiders/rentit.py
@@ -5,7 +5,7 @@ import scrapy
 class RentitSpider(scrapy.Spider):
     name = 'rentit'
     allowed_domains = ['otodom.pl']
-    start_urls = ['http://otodom.pl/']
+    start_urls = ['http://otodom.pl/sprzedaz/mieszkanie/warszawa/']
 
     def parse(self, response):
         pass
