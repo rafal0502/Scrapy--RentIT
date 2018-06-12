93da88db31968f812a1bd85bda98dd7790337f12
rafal0502 <rafalchojnacki0502@gmail.com>
2018-06-12 22:33:12 +0200

    Add new categories, changed selectors

--- Scrapy spider RentIT/RentIT_crawler/RentIT_crawler/spiders/rentit.py
+++ Scrapy spider RentIT/RentIT_crawler/RentIT_crawler/spiders/rentit.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.http import Request
-# from geopy.geocoders import Nominatim
+
 
 
 class RentitSpider(scrapy.Spider):
@@ -20,25 +20,27 @@ class RentitSpider(scrapy.Spider):
 
     def parse_flat(self, response):
         tytul = response.xpath('//*[@class="col-md-offer-content"]//h1//text()').extract_first()
-        cena = response.xpath('//*[@class="param_price"]//strong/text()').extract_first()
+        cena = response.xpath('//*[@class="params-list"]//*[@class="main-list"]//li//strong//text()').extract()[0]
         cena_za_metr = response.xpath('//*[@class="param_price"]/text()')[1].extract()
-        powierzchnia = response.xpath('//*[@class="param_m"]//strong/text()').extract_first()
-        pokoje = response.xpath('//strong/text()')[7].extract()
-        pietro =  response.xpath('//*[@class="param_floor_no"]//text()')[1].extract()
-
+        powierzchnia = response.xpath('//*[@class="params-list"]//*[@class="main-list"]//li//strong//text()').extract()[1]
+        pokoje = response.xpath('//*[@class="params-list"]//*[@class="main-list"]//li//strong//text()').extract()[2]
+        pietro = response.xpath('//*[@class="params-list"]//*[@class="main-list"]//li//strong//text()').extract()[3]
         sublista = response.xpath('//*[@class="sub-list"]//li//text()').extract()
         sublista_dict = {sublista[i] : sublista[i+1] for i in range(0, len(sublista), 2)}
-
         zdjecia = response.xpath('//*[@class="col-md-offer-content"]//img/@src')[:5].extract()
         wspolrzedne_lat = response.xpath('//*[@class="ad-map-element"]//@data-lat').extract_first()
         wspolrzedne_lon = response.xpath('//*[@class="ad-map-element"]//@data-lon').extract_first()
-        
-
         informacje = response.xpath('//*[@class="dotted-list"]//li//text()').extract()
         informacje_dodatkowe = ''.join(informacje)
         opis = response.xpath('//*[@itemprop="description"]//p//text()').extract()
         opis_finalny = ''.join(opis)
-
+        dzielnica = response.xpath('//*[@class="p-t-5"]//a//text()').extract()[2]
+        strona = "www.otodom.pl"
+        nr_oferty_w_otodom = response.xpath('//*[@class="left"]//p//text()').extract()[0][20:]
+        nr_oferty_w_biurze_nieruchomosci = response.xpath('//*[@class="left"]//p//text()').extract()[1][34:]
+        liczba_wyswietlen = response.xpath('//*[@class="left"]//p//text()').extract()[2].strip()[26:]
+        data_dodania = response.xpath('//*[@class="right"]//p//text()').extract()[0][14:]
+        data_aktualizacji = response.xpath('//*[@class="right"]//p//text()').extract()[1][19:]
 
 
         yield {
@@ -54,4 +56,11 @@ class RentitSpider(scrapy.Spider):
             'opis_finalny': opis_finalny,
             'wspolrzedne_lat': wspolrzedne_lat,
             'wspolrzedne_lon': wspolrzedne_lon,
-        }
+            'dzielnica': dzielnica,
+            'strona': strona,
+            'nr_oferty_w_biurze_nieruchomosci': nr_oferty_w_biurze_nieruchomosci,
+            'nr_oferty_w_otodom': nr_oferty_w_otodom,
+            'liczba_wyswietlen': liczba_wyswietlen,
+            'data_dodania': data_dodania,
+            'data_aktualizacji': data_aktualizacji,
+       }
